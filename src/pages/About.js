cd21f0c5e6fb3e5969ffd9d388973cf8ea790dc6
franckdun <scorven7@gmail.com>
2022-05-18 02:32:26 +0200

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos molestias dolor animi excepturi error earum soluta perferendis non hic numquam quae eligendi voluptatem deleniti, maxime odit explicabo modi tempora perspiciatis dolorem voluptatibus! Temporibus, quo? Sunt dolorum nemo, labore laudantium aliquam necessitatibus ullam odio cumque impedit in iure saepe sit! Suscipit, ea provident error, hic a explicabo quod sit dignissimos fugiat nisi in recusandae! Pariatur eligendi numquam ipsa architecto hic necessitatibus, cumque molestiae dolorem iste praesentium ipsum ex, sed modi delectus esse assumenda autem qui unde vel. Quis reiciendis explicabo eaque, eius quod iste ipsam totam neque minima recusandae, quasi numquam?</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia illum maiores unde reprehenderit! Dolor voluptates neque laudantium alias distinctio eum quidem sunt, aliquam nam aperiam amet ut praesentium perspiciatis in provident! Modi harum nostrum voluptatibus voluptate dolores ipsa aspernatur recusandae, illo iste beatae magnam molestias maxime inventore adipisci sit. Exercitationem facere reiciendis incidunt! Nisi ratione est blanditiis voluptas a, exercitationem similique. Ipsum expedita quos sunt fuga maxime vero quia dolorum sequi, neque cupiditate suscipit non fugiat est cum! Repudiandae sequi optio, sit et consectetur enim non facere facilis suscipit aspernatur natus, laudantium consequatur quisquam necessitatibus. Aperiam dicta nihil quas ullam at. Molestias velit iusto quis ipsa? Quam, debitis delectus accusantium ipsa voluptate cupiditate ex illum deserunt beatae iste magnam nemo, odit nihil aliquam modi tempore fugit. Illo omnis officiis perferendis laudantium tenetur? Provident quia at ex iste quae ipsa laboriosam, sed perspiciatis! Amet dolores delectus repudiandae et ea aut repellat saepe rerum harum reprehenderit alias fugiat debitis facilis ut optio, ad sed enim quibusdam doloremque itaque, minus, dolorum cum beatae molestias? Iste dicta incidunt quo omnis, expedita impedit qui cupiditate laudantium repudiandae rerum eum asperiores enim cum placeat necessitatibus aliquid fugiat eius voluptatem exercitationem voluptates laboriosam debitis voluptas architecto. Fugiat.</p>
+        </div>
+    );
+};
+
+export default About;
